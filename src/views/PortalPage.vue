0bd17476d4ce483843fbb2e8e6b83f659de7ec74
YifanFei <ivanfee@outlook.com>
2022-04-08 13:55:53 +0800

    fix: improve portal page ui

--- src/views/PortalPage.vue
+++ src/views/PortalPage.vue
@@ -1,13 +1,13 @@
 <template>
   <v-container>
-    <v-row style="margin-top: 28vh">
+    <v-row style="margin-top: 30vh">
       <v-col cols="4"></v-col>
       <v-col cols="4" style="text-align: center"><h1>请输入课程名称</h1></v-col>
       <v-col cols="4"></v-col>
     </v-row>
-    <v-row no-gutters class="mt-3">
+    <v-row no-gutters>
       <v-col cols="2">
-        <v-select label="搜索类别" filled outlined class="rounded-r-0"></v-select>
+        <v-select label="搜索类别" outlined class="rounded-r-0"></v-select>
       </v-col>
       <v-col>
         <v-combobox outlined class="rounded-l-0"></v-combobox>
