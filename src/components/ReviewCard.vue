7b5933c2bf43677c1602b1117f341a44887ed0fc
YifanFei <ivanfee@outlook.com>
2022-04-10 17:10:49 +0800

    fix: improve review card ui

--- src/components/ReviewCard.vue
+++ src/components/ReviewCard.vue
@@ -3,15 +3,22 @@
     <v-card>
       <v-row no-gutters class="d-flex justify-space-between">
         <v-card class="pa-0 ma-0" flat>
-          <v-card-title class="text-h5 font-weight-bold"> {{ review.review.title }} </v-card-title>
-          <v-card-subtitle class="pb-0"> {{ review.course.teachers }} (2021-2022-2)</v-card-subtitle>
+          <v-card-title class="text-h5 font-weight-bold pt-3 pb-2"> {{ review.review.title }} </v-card-title>
+          <v-row align="center" justify="start" class="ml-3 pb-3">
+            <v-col class="shrink pr-0 pl-1">
+              <v-chip small label outlined color="blue"><v-icon small> mdi-account-outline </v-icon>{{ review.course.teachers }}</v-chip>
+            </v-col>
+            <v-col class="shrink pr-0 pl-2">
+              <v-chip small label outlined color="red"><font-awesome-icon icon="fa-solid fa-calendar-days" />&nbsp; 2011-2012</v-chip>
+            </v-col>
+          </v-row>
         </v-card>
         <v-card style="background-color: rgba(0, 0, 0, 0.03)" flat class="d-flex">
           <v-divider vertical />
           <v-card class="pa-0 ma-0 mx-7 d-flex justify-space-between" flat>
             <div class="pl-1" style="background-color: rgba(0, 0, 0, 0.03)">
               <v-row style="font-size: 30px" class="mt-2 grey--text align-self-start">
-                <font-awesome-icon icon="fa-solid fa-caret-up" />
+                <font-awesome-icon icon="fa-solid fa-caret-up" class="blue--text" />
               </v-row>
               <v-row class="text-h7 mt-1 align-self-center"> {{ review.review.remark }} </v-row>
               <v-row style="font-size: 30px" class="mt-0 pt-1 mb-1 grey--text align-self-end">
@@ -32,8 +39,9 @@
         </v-col>
         <v-col cols="9" style="text-align: right" class="pr-3">
           <v-card-text class="pa-1 caption">
-            总评分 <font-awesome-icon icon="fa-solid fa-face-grin-beam" /> &nbsp;课程内容 <font-awesome-icon icon="fa-solid fa-face-meh" />&nbsp;工作量
-            <font-awesome-icon icon="fa-solid fa-face-frown" />&nbsp;考核 <font-awesome-icon icon="fa-solid fa-face-sad-tear" />
+            总评分 <font-awesome-icon class="orange--text" icon="fa-solid fa-face-grin-beam" /> &nbsp;课程内容
+            <font-awesome-icon class="grey--text" icon="fa-solid fa-face-meh" />&nbsp;工作量 <font-awesome-icon icon="fa-solid fa-face-frown" />&nbsp;考核
+            <font-awesome-icon class="red--text" icon="fa-solid fa-face-sad-tear" />
           </v-card-text>
         </v-col>
       </v-row>
