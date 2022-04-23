3ecce1da251e1e7ed869a65e4e17f9150278b036
YifanFei <ivanfee@outlook.com>
2022-04-23 15:39:57 +0800

    fix: add auth url to config and add interceptor for 401

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,75 +1,62 @@
-import {
-  Course,
-  CourseGroup,
-  ICourse,
-  ICourseData,
-  ICourseGroup,
-  IReview,
-  IReviewData,
-  Review,
-} from "@/models";
-import axios from "@/utils/axios";
-import config from "@/config";
-import { camelizeKeys } from "@/utils";
+import { Course, CourseGroup, ICourse, ICourseData, ICourseGroup, IReview, IReviewData, Review } from '@/models'
+import axios from '@/utils/axios'
+import config from '@/config'
+import { camelizeKeys } from '@/utils'
 export const addCourse = async (courseData: ICourseData) => {
-  const response = await axios.post("/courses", courseData);
-  const data: ICourse = camelizeKeys(response.data);
-  return new Course(data);
-};
+  const response = await axios.post('/courses', courseData)
+  const data: ICourse = camelizeKeys(response.data)
+  return new Course(data)
+}
 
 export const getCourseGroups = async () => {
-  const response = await axios.get("/courses");
-  const data: ICourseGroup[] = camelizeKeys(response.data);
-  return data.map((v) => new CourseGroup(v));
-};
+  const response = await axios.get('/courses')
+  const data: ICourseGroup[] = camelizeKeys(response.data)
+  return data.map((v) => new CourseGroup(v))
+}
 
 export const getCourseGroup = async (groupId: number) => {
-  const response = await axios.get(`/group/${groupId}`);
-  const data: ICourseGroup = camelizeKeys(response.data);
-  return new CourseGroup(data);
-};
+  const response = await axios.get(`/group/${groupId}`)
+  const data: ICourseGroup = camelizeKeys(response.data)
+  return new CourseGroup(data)
+}
 export const getCourse = async (courseId: number) => {
-  const response = await axios.get(`/courses/${courseId}`);
-  const data: ICourse = camelizeKeys(response.data);
-  return new Course(data);
-};
+  const response = await axios.get(`/courses/${courseId}`)
+  const data: ICourse = camelizeKeys(response.data)
+  return new Course(data)
+}
 
 export const getReviews = async (courseId: number) => {
-  const response = await axios.get(`/courses/${courseId}/reviews`);
-  const data: IReview[] = camelizeKeys(response.data);
-  return data.map((v) => new Review(v));
-};
+  const response = await axios.get(`/courses/${courseId}/reviews`)
+  const data: IReview[] = camelizeKeys(response.data)
+  return data.map((v) => new Review(v))
+}
 
 export const addReview = async (courseId: number, reviewData: IReviewData) => {
-  const response = await axios.post(`/courses/${courseId}/reviews`, reviewData);
-  const data: IReview = camelizeKeys(response.data);
-  return new Review(data);
-};
+  const response = await axios.post(`/courses/${courseId}/reviews`, reviewData)
+  const data: IReview = camelizeKeys(response.data)
+  return new Review(data)
+}
 
 export const removeReview = async (reviewId: number): Promise<string> => {
-  const response = await axios.delete(`/reviews/${reviewId}`);
-  return response.data;
-};
+  const response = await axios.delete(`/reviews/${reviewId}`)
+  return response.data
+}
 
-export const modifyReview = async (
-  reviewId: number,
-  reviewData: IReviewData
-) => {
-  const response = await axios.put(`/reviews/${reviewId}`, reviewData);
-  const data: IReview = camelizeKeys(response);
-  return new Review(data);
-};
+export const modifyReview = async (reviewId: number, reviewData: IReviewData) => {
+  const response = await axios.put(`/reviews/${reviewId}`, reviewData)
+  const data: IReview = camelizeKeys(response)
+  return new Review(data)
+}
 
 // jump to login page when return 401
 axios.interceptors.response.use(
   (response) => {
-    return response;
+    return response
   },
   (error) => {
     if (error.response.status === 401) {
-      window.location.href =
-        config.authBaseUrl + "login?url=" + location.origin;
+      window.location.href = config.authBaseUrl + 'login?url=' + location.origin
     }
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
