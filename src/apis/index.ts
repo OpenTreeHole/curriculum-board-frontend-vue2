c988388c250cfe28d409ec2653719ecd8c01bbd6
Dest1n1 <648377563@qq.com>
2022-04-25 15:55:03 +0800

    create: jwt
    Signed-off-by: Dest1n1 <648377563@qq.com>

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,7 +1,135 @@
 import { Course, CourseGroup, ICourse, ICourseData, ICourseGroup, IReview, IReviewData, Review } from '@/models'
-import axios from '@/utils/axios'
+import axios, { authAxios, jwt, refreshAxios } from '@/apis/axios'
 import config from '@/config'
-import { camelizeKeys } from '@/utils'
+import { camelizeKeys, snakifyKeys } from '@/utils'
+import { IPunishment, IUserAuth, IUserAuthData, Punishment, UserAuth } from '@/models/user'
+import Cookies from 'js-cookie'
+
+jwt.refresh = async () => (await refresh()).access
+
+export const login = async (email: string, password: string): Promise<{ message: string; access: string; refresh: string }> => {
+  const response = await authAxios.post('/login', {
+    email: email,
+    password: password
+  })
+  Cookies.set('access', response.data.access, { domain: config.cookieDomain, expires: 10 })
+  Cookies.set('refresh', response.data.refresh, { domain: config.cookieDomain, expires: 10 })
+  return camelizeKeys(response.data)
+}
+
+export const logout = async (): Promise<{ message: string }> => {
+  const response = await authAxios.get('/logout')
+  return camelizeKeys(response.data)
+}
+
+export const refresh = async (): Promise<{ message: string; access: string; refresh: string }> => {
+  const response = await refreshAxios.post('/refresh')
+  Cookies.set('access', response.data.access, { domain: config.cookieDomain, expires: 10 })
+  Cookies.set('refresh', response.data.refresh, { domain: config.cookieDomain, expires: 10 })
+  return camelizeKeys(response.data)
+}
+
+export const verifyWithEmail = async (email: string): Promise<{ message: string; scope: string }> => {
+  const response = await authAxios.get('/verify/email', {
+    params: {
+      email: email
+    }
+  })
+  return camelizeKeys(response.data)
+}
+
+export const register = async (password: string, email: string, verification: string): Promise<{ message: string; access?: string; refresh?: string }> => {
+  const response = await authAxios.post('/register', {
+    password: password,
+    email: email,
+    verification: verification
+  })
+  Cookies.set('access', response.data.access, { domain: config.cookieDomain, expires: 10 })
+  Cookies.set('refresh', response.data.refresh, { domain: config.cookieDomain, expires: 10 })
+  return camelizeKeys(response.data)
+}
+
+export const changePassword = async (password: string, email: string, verification: string): Promise<{ message: string; access?: string; refresh?: string }> => {
+  const response = await authAxios.put('/register', {
+    password: password,
+    email: email,
+    verification: verification
+  })
+  Cookies.set('access', response.data.access, { domain: config.cookieDomain, expires: 10 })
+  Cookies.set('refresh', response.data.refresh, { domain: config.cookieDomain, expires: 10 })
+  return camelizeKeys(response.data)
+}
+
+export const listPunishmentsByUser = async (userId: number) => {
+  const response = await authAxios.get(`/users/${userId}/punishments`)
+  const data: IPunishment[] = camelizeKeys(response.data)
+  return data.map((v) => new Punishment(v))
+}
+
+export const addPunishment = async (userId: number, reason: string, days: number, scope: string) => {
+  const response = await authAxios.post(`/users/${userId}/punishments`, {
+    reason: reason,
+    days: days,
+    scope: scope
+  })
+  return new Punishment(camelizeKeys(response.data))
+}
+
+export const getPunishmentByUser = async (userId: number, id: number) => {
+  const response = await authAxios.get(`/users/${userId}/punishments/${id}`)
+  return new Punishment(camelizeKeys(response.data))
+}
+
+export const getPunishmentById = async (id: number) => {
+  const response = await authAxios.get(`/punishments/${id}`)
+  return new Punishment(camelizeKeys(response.data))
+}
+
+export const listPunishments = async (size: number, offset: number) => {
+  const response = await authAxios.get('/punishments', {
+    params: {
+      size: size,
+      offset: offset
+    }
+  })
+  const data: IPunishment[] = camelizeKeys(response.data)
+  return data.map((v) => new Punishment(v))
+}
+
+export const getCurrentUser = async () => {
+  const response = await authAxios.get('/users/me')
+  return new UserAuth(camelizeKeys(response.data))
+}
+
+export const deleteUser = async (password: string) => {
+  await authAxios.delete('/users/me', {
+    data: {
+      password: password
+    }
+  })
+}
+
+export const getUserById = async (userId: number) => {
+  const response = await authAxios.get(`/users/${userId}`)
+  return new UserAuth(camelizeKeys(response.data))
+}
+
+export const modifyUser = async (userId: number, userData: IUserAuthData) => {
+  const response = await authAxios.put(`/users/${userId}`, snakifyKeys(userData))
+  return new UserAuth(camelizeKeys(response.data))
+}
+
+export const listUsers = async (size: number, offset: number) => {
+  const response = await authAxios.get('/users', {
+    params: {
+      size: size,
+      offset: offset
+    }
+  })
+  const data: IUserAuth[] = camelizeKeys(response.data)
+  return data.map((v) => new UserAuth(v))
+}
+
 export const addCourse = async (courseData: ICourseData) => {
   const response = await axios.post('/courses', courseData)
   const data: ICourse = camelizeKeys(response.data)
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import camelCase from 'lodash-es/camelCase'
+import { snakeCase } from 'lodash-es'
 
 export const camelizeKeys = (obj: any): any => {
   if (Array.isArray(obj)) {
@@ -8,6 +9,21 @@ export const camelizeKeys = (obj: any): any => {
       (result, key) => ({
         ...result,
         [camelCase(key)]: camelizeKeys(obj[key])
+      }),
+      {}
+    )
+  }
+  return obj
+}
+
+export const snakifyKeys = (obj: any): any => {
+  if (Array.isArray(obj)) {
+    return obj.map((v) => snakifyKeys(v))
+  } else if (obj != null && obj.constructor === Object) {
+    return Object.keys(obj).reduce(
+      (result, key) => ({
+        ...result,
+        [snakeCase(key)]: snakifyKeys(obj[key])
       }),
       {}
     )
