05ee70b3810de4516b8c3d3d7be2585b4a6b177f
w568w <1278297578@qq.com>
2022-04-26 10:47:40 +0800

    migrate: to vite (not fully tested)

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,16 @@ import vuetify from './plugins/vuetify'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Vuetify from 'vuetify'
 
 Vue.config.productionTip = false
 
 library.add(fas)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+// 2022-4-26 (@w568w): why do we need to re-invoke Vue.use here?
+// It seems that even commenting out this line will break the build.
+Vue.use(Vuetify)
 
 new Vue({
   router,
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib/framework'
-
+import Vuetify from 'vuetify/lib'
+// 2022-4-26 (@w568w): Is it necessary to import it from 'vuetify/lib', rather than 'vuetify/lib/framework' or 'vuetify'?
+// From the source all of them are semantically identical. Is there anything to do with tree-shaking process?
 Vue.use(Vuetify)
 
 export default new Vuetify({
