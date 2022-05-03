e587a07442423389cdfc9f6adc1e3d5dc5a729b3
YifanFei <ivanfee@outlook.com>
2022-05-03 14:02:14 +0800

    fix: fix backend api

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  backendAPI: process.env.NODE_ENV !== 'production' ? 'https://testcb.fduhole.com/' : '/api/',
+  backendAPI: process.env.VUE_APP_AUTH_BASE_URL,
   authUrl: process.env.VUE_APP_AUTH_URL,
   authBaseUrl: process.env.VUE_APP_AUTH_BASE_URL,
   cookieDomain: process.env.VUE_APP_COOKIE_DOMAIN
