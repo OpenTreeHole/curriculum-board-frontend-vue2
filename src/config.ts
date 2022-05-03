6a04ba46c80f689dd6fcc6169cbfeedacf2f7c2e
Dest1n1 <dest1nis2022@gmail.com>
2022-05-03 14:23:28 +0800

    fix: read env
    Signed-off-by: Dest1n1 <dest1nis2022@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 const config = {
-  backendAPI: process.env.VUE_APP_AUTH_BASE_URL,
-  authUrl: process.env.VUE_APP_AUTH_URL,
-  authBaseUrl: process.env.VUE_APP_AUTH_BASE_URL,
-  cookieDomain: process.env.VUE_APP_COOKIE_DOMAIN
+  backendAPI: process.env.VITE_BACKEND_API,
+  authUrl: process.env.VITE_AUTH_URL,
+  authBaseUrl: process.env.VITE_AUTH_BASE_URL,
+  cookieDomain: process.env.VITE_COOKIE_DOMAIN
 }
 
 export default config
