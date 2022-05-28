7196cc652bcf2973b947e26746198bdfbedef121
ivanfei <ivanfee@outlook.com>
2022-05-28 13:46:28 +0800

    test done: remove log

--- src/components/ReviewCard.vue
+++ src/components/ReviewCard.vue
@@ -289,7 +289,6 @@ export default Vue.extend({
     }
   },
   mounted() {
-    console.log(11111)
     this.viewer = new Viewer({
       el: this.$refs['reviewContent' + this.review.review.id] as HTMLElement,
       initialValue: this.review.review.content
