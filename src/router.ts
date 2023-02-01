0acb9e9ba7210e0d61e59fd4eaf6dbc41978d1d5
ivanfei <ivanfee@outlook.com>
2023-02-02 02:20:21 +0800

    feature: route with token included in Url - close #15

--- src/router.ts
+++ src/router.ts
@@ -40,6 +40,18 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
+  // 如果url包含/jump?access={1}&refresh={2}
+  if (to.fullPath && to.fullPath.includes('/jump?access=')) {
+    // 从url中提取access和refresh token 并编辑 to.path
+    const access = to.fullPath.split('=')[1].split('&')[0]
+    const refresh = to.fullPath.split('=')[2]
+    const path = to.fullPath.split('/jump?access=')[0]
+    // 将token存入cookie
+    Cookies.set('access', access)
+    Cookies.set('refresh', refresh)
+    // 跳转到刚才要去的页面
+    next(path)
+  }
   if (to.meta?.title) {
     document.title = to.meta.title
   }
